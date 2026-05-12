a5368407b7393dbe8ec6862d495d098b86e41c30
Tanushri Sapate <your-email@example.com>
2026-05-12 22:52:07 +0530

    Initial  frontend and backend added

---
+++ backend/app/core/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://postgres:Tanu2920#@localhost:5432/Pincher"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ backend/app/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from app.core.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    email = Column(String, unique=True, index=True)
+
+    username = Column(String, unique=True)
+
+    password = Column(String)
